b8c854d37b65f15ac74a17baea8469184ff9c7fa
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-08-07 16:48:12 -0400

    fix

--- parse.js
+++ parse.js
@@ -1,7 +1,7 @@
 import Lexer from './src/lexer.js';
 import Parser from './src/parser.js';
 
-console.time('Execution time');
 const code = Deno.readTextFileSync(Deno.args[Deno.args.length - 1]);
+console.time('Execution time');
 console.log(JSON.stringify(new Parser(new Lexer(code)), null, 4));
 console.timeEnd('Execution time');
